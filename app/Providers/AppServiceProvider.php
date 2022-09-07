107625801516abb125bbb900a5f11fe8714528d8
Lama Bakour <106868044+lamaadebbakour@users.noreply.github.com>
2022-09-08 00:18:32 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Gate::define('admin', function (User $user) {
-            return $user->name === 'lama';
-         });
-    }
+       
 }
